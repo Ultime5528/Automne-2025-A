357efa28862b6c7284d7b4670f78ffe40adb6d4d
Edmond <4201901@keranna.qc.ca>
2025-11-19 18:24:28 -0500

    WIP GroupCommand Charge

--- commands/slingshot/charge.py
+++ commands/slingshot/charge.py
@@ -1,30 +1,19 @@
-import wpilib
+from commands2 import SequentialCommandGroup
 
-from ports import slingshot_motor
-from ultime.command import Command
+from commands.slingshot.movedown import MoveDown
+from commands.slingshot.moveup import MoveUp
+from commands.slingshot.unlock import Unlock
+from commands.slingshot.lock import Lock
 from subsystems.slingshot import Slingshot
-from ultime.autoproperty import autoproperty
 
 
-class Embobiner(Command):
-    duration = autoproperty(0.5)
-
-
-    def __init__(self, spool: Slingshot):
-        super().__init__()
-        self.spool = spool
-        self.timer = wpilib.Timer()
-        self.addRequirements(self.spool)
-
-    def initialize(self):
-        self.timer.restart()
-
-
-    def execute(self):
-        self.spool.moveUp()
-
-    def isFinished(self) -> bool:
-        return self.timer.hasElapsed(self.duration)
-
-    def end(self, interrupted: bool):
-        self.spool.stop()
+class Charge(SequentialCommandGroup):
+    def __init__(self, slingshot: Slingshot):
+        super().__init__(
+            Unlock(slingshot),
+            MoveUp(slingshot),
+            MoveDown(slingshot),
+            Lock(slingshot),
+            MoveUp(slingshot),
+            MoveUp(slingshot)
+        )
